b217119589934dfadaf61fd17fd40c4f39ec63a8
Retsam <chess19@gmail.com>
2019-12-30 17:29:28 -0500

    Day 11 solved

---
+++ 2019/day11/src/grid.rs
@@ -0,0 +1,92 @@
+use std::collections::HashMap;
+use std::cmp::{min, max};
+
+pub trait GridCode {
+    fn grid_code(&self) -> char;
+}
+
+struct GridBounds {
+    min_x: i32,
+    max_x: i32,
+    min_y: i32,
+    max_y: i32,
+}
+
+fn boundaries<T>(grid: &HashMap<Coord, T>) -> GridBounds {
+    grid.keys().fold(GridBounds {
+        min_x: 0, min_y: 0,
+        max_x: 0, max_y: 0,
+    }, |mut bounds, coord| {
+        bounds.min_x = min(bounds.min_x, coord.x);
+        bounds.min_y = min(bounds.min_y, coord.y);
+        bounds.max_x = max(bounds.max_x, coord.x);
+        bounds.max_y = max(bounds.max_y, coord.y);
+        bounds
+    })
+}
+
+pub fn print<T: GridCode>(grid: &HashMap<Coord, T>, default: &T) -> String {
+    let bounds = boundaries(grid);
+    let mut out = String::new();
+
+    for y in bounds.min_y..=bounds.max_y {
+        for x in bounds.min_x..=bounds.max_x {
+            let tile = grid.get(&Coord::new(x, y)).unwrap_or(default);
+            out.push(tile.grid_code())
+        }
+        out.push('\n');
+    }
+    out
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Dir { Up, Down, Left, Right }
+use Dir::{*};
+
+impl Dir {
+    pub fn cw(self) -> Dir {
+        match self {
+            Up => Right,
+            Right => Down,
+            Down => Left,
+            Left => Up,
+        }
+    }
+    pub fn ccw(self) -> Dir {
+        match self {
+            Up => Left,
+            Right => Up,
+            Down => Right,
+            Left => Down,
+        }
+    }
+    // pub fn rev(self) -> Dir {
+    //     match self {
+    //         Up => Down,
+    //         Down => Up,
+    //         Right => Left,
+    //         Left => Right,
+    //     }
+    // }
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
+pub struct Coord {
+    pub x: i32,
+    pub y: i32,
+}
+impl Coord {
+    pub fn new(x: i32, y: i32) -> Coord {
+        Coord { x, y }
+    }
+    pub fn go(&self, dir: Dir) -> Coord {
+        let x = self.x;
+        let y = self.y;
+        match dir {
+            Up    => Coord { x, y: y - 1 },
+            Down  => Coord { x, y: y + 1 },
+            Left  => Coord { x: x - 1, y },
+            Right => Coord { x: x + 1, y },
+        }
+    }
+}
---
+++ 2019/day11/src/main.rs
@@ -0,0 +1,77 @@
+use std::io::{self, Read};
+mod grid;
+
+use intcode::{ IntcodeProgram, RunResult };
+use std::collections::HashMap;
+use std::collections::HashSet;
+use grid::{Coord, Dir, GridCode};
+
+use Dir::{*};
+
+const PART_2: bool = true;
+
+#[derive(Debug)]
+enum Color { Black, White }
+use Color::{*};
+
+impl GridCode for Color {
+    fn grid_code(&self) -> char {
+        match self { Black => ' ', White => '#' }
+    }
+}
+
+struct PaintingState {
+    grid: HashMap<Coord, Color>,
+    painted: HashSet<Coord>,
+    position: Coord,
+    dir: Dir,
+}
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    let line = buffer.lines().next().unwrap();
+    let mut prog = IntcodeProgram::from_str(line);
+
+    let mut state = PaintingState {
+        grid: HashMap::new(),
+        painted: HashSet::new(),
+        position: Coord::new(0, 0),
+        dir: Up,
+    };
+    if PART_2 {
+        state.grid.insert(Coord::new(0, 0), White);
+    }
+
+    loop {
+        match prog.run() {
+            RunResult::Halted => break,
+            RunResult::AwaitingInput => {
+                let input = match state.grid.entry(state.position).or_insert(Black) {
+                    Black => 0,
+                    White => 1,
+                };
+                prog.add_input(&[input]);
+            },
+            RunResult::Output(color_val) => {
+                let color = if color_val == 0 { Black } else { White };
+                state.painted.insert(state.position.clone());
+                state.grid.insert(state.position.clone(), color);
+                if let RunResult::Output(turn_val) = prog.run() {
+                    state.dir = if turn_val == 0 {
+                        state.dir.ccw()
+                    } else  {
+                        state.dir.cw()
+                    };
+                    state.position = state.position.go(state.dir);
+                } else {
+                    panic!("Got color without turn");
+                };
+            }
+        }
+    }
+
+    println!("Painted {} tiles", state.painted.len());
+    println!("{}", grid::print(&state.grid, &Black));
+    Ok(())
+}
