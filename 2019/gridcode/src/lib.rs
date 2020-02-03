edd3acf10e9741a3ceba504beafc1b66c3714fc1
Retsam <chess19@gmail.com>
2020-02-02 20:01:47 -0500

    Add find_path

--- 2019/gridcode/src/lib.rs
+++ 2019/gridcode/src/lib.rs
@@ -1,8 +1,16 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::cmp::{min, max};
+use core::fmt;
+
+pub type Grid<T> = HashMap<Coord, T>;
 
 pub trait GridCode {
     fn grid_code(&self) -> char;
+}
+impl GridCode for char {
+    fn grid_code(&self) -> char { *self }
 }
 
 struct GridBounds {
@@ -42,6 +50,36 @@ pub fn print_with_overrides<T: GridCode>(grid: &HashMap<Coord, T>, overrides: &H
         out.push('\n');
     }
     out
+}
+
+pub fn find_path<T: GridCode, F>(grid: &Grid<T>, from: Coord, to: Coord, is_open: F) -> Option<Vec<Coord>>
+    where F: Fn(&T) -> bool {
+    let mut search_queue = VecDeque::new();
+    let mut searched = HashSet::new();
+    search_queue.push_back((vec!(), from));
+    loop {
+        if let Some((path, node)) = search_queue.pop_front() {
+            searched.insert(node); // Technically could be done when added to queue
+            let neighbors = node.neighbors();
+            let mut new_path = path.clone();
+            new_path.push(node);
+            if neighbors.contains(&to) {
+                new_path.push(to);
+                new_path.drain(..1); //Remove first item
+                break Some(new_path);
+            }
+
+            let new_search: Vec<_> = neighbors.into_iter()
+                .filter(|c| grid.get(c).map(|x| is_open(x)).unwrap_or(false))
+                .filter(|c| !searched.contains(c))
+                .map(|c| (new_path.clone(), *c))
+                .collect();
+            search_queue.append(&mut VecDeque::from(new_search));
+        } else {
+            break None;
+        }
+
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -96,5 +134,23 @@ impl Coord {
     }
     pub fn neighbors(&self) -> [Coord; 4] {
         [self.go(Up), self.go(Right), self.go(Down), self.go(Left)]
+    }
+}
+impl fmt::Display for Coord {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{*};
+    #[test]
+    fn find_path_test() {
+        let path = vec!(Coord::new(0, 1), Coord::new(1,1), Coord::new(2,1));
+        let grid: Grid<char> = path.iter().cloned().map(|c| (c, '.')).collect();
+        assert_eq!(path,
+            find_path(&grid, Coord::new(0, 0),  Coord::new(2,1), |_| true).unwrap()
+        )
     }
 }
