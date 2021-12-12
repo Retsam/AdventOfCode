ea06999a1f898b1ea92fb9febb92b5331d792998
Retsam <chess19@gmail.com>
2021-12-12 08:23:43 -0500

    Simplify day04 a bit

--- 2021/day04/day4.hs
+++ 2021/day04/day4.hs
@@ -1,7 +1,5 @@
 import Data.Char (isNumber, isSpace)
-import Data.Either (fromLeft, isLeft)
 import Data.List (find, transpose)
-import Data.Maybe (fromJust)
 import Data.Set (Set, member)
 import qualified Data.Set as Set
 import Text.ParserCombinators.ReadP (ReadP, char, eof, munch, munch1, readP_to_S, sepBy, sepBy1, skipSpaces, string)
@@ -16,13 +14,6 @@ main = do
   print $ part2 initState
 
 -- part 1
-
-part1 :: State -> Int
-part1 initState =
-  fromLeft (error "Expected a score") result
-  where
-    result = sequence $ iterateM step (Right initState)
-
 wins :: Set Int -> Board -> Bool
 wins drawn board = any lineWins board || any lineWins (transpose board)
   where
@@ -30,14 +21,14 @@ wins drawn board = any lineWins board || any lineWins (transpose board)
 
 type State = (Set Int, [Int], [Board])
 
-step :: State -> Either Int State
-step (drawn, toCall : rest, boards) =
+part1 :: State -> Int
+part1 (drawn, toCall : rest, boards) =
   case find (wins newDrawn) boards of
-    Just board -> Left (toCall * score newDrawn board)
-    Nothing -> Right (newDrawn, rest, boards)
+    Just board -> toCall * score newDrawn board
+    Nothing -> part1 (newDrawn, rest, boards)
   where
     newDrawn = Set.insert toCall drawn
-step (_, [], _) = error "Ran out of numbers"
+part1 (_, [], _) = error "Ran out of numbers"
 
 score :: Set Int -> [[Int]] -> Int
 score drawn = sum . filter (not . (`member` drawn)) . concat
@@ -82,10 +73,3 @@ runParser parser input =
     [(xs, "")] -> xs
     [] -> error "Input parse error"
     _ -> error "Didn't consume whole input"
-
---- utils
-
-iterateM :: (Monad m) => (a -> m a) -> m a -> [m a]
-iterateM f init =
-  let m = init >>= f
-   in m : iterateM f m
