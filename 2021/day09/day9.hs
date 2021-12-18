86a03a52c8d8ca61cb18c7fb5c7f77c034a64153
Retsam <chess19@gmail.com>
2021-12-18 15:38:04 -0500

    Day 9 part 2

--- 2021/day09/day9.hs
+++ 2021/day09/day9.hs
@@ -1,23 +1,51 @@
 import Data.Bifunctor (Bifunctor (bimap))
-import Data.Char
+import Data.Char (digitToInt)
+import Data.List
 import Data.Maybe (isJust)
+import qualified Data.Set as S
 
 main = do
   input <-
     getContents
   let hmap = map (map digitToInt) (lines input)
 
-  print $ part1 hmap
+  print $ part2 hmap
 
-flatMap func list = list >>= func
-
-part1 :: [[Int]] -> Int
-part1 hmap = sum $ map sum $ mapGridWithCoords isBasin hmap
+isBasin :: [[Int]] -> (Int, Int) -> Bool
+isBasin hmap coords = all (> valueAt hmap coords) $ neighborValues coords
   where
     neighborValues = map (valueAt hmap) . neighbors hmap
-    isBasin coords v = if all (> v) $ neighborValues coords then v + 1 else 0
+
+part1 :: [[Int]] -> Int
+part1 hmap = sum $ map sum $ mapGridWithCoords riskValue hmap
+  where
+    riskValue coords v = if isBasin hmap coords then v + 1 else 0
+
+part2 :: [[Int]] -> Int
+part2 hmap = part2Score $ map (basinSize hmap) $ filter (isBasin hmap) $ gridCoords hmap
+
+part2Score :: [Int] -> Int
+part2Score = product . take 3 . reverse . sort
+
+basinSize :: [[Int]] -> (Int, Int) -> Int
+basinSize hmap coord = fst $ _basinSize (S.singleton coord) hmap coord
+
+_basinSize :: S.Set (Int, Int) -> [[Int]] -> (Int, Int) -> (Int, S.Set (Int, Int))
+_basinSize visited hmap coord = foldl (floodAcc hmap) (1, newVisited) toVisit
+  where
+    newVisited = S.union visited (S.fromList toVisit)
+    shouldVisit coord = not (S.member coord visited || valueAt hmap coord == 9)
+    toVisit = filter shouldVisit $ neighbors hmap coord
+
+floodAcc hmap (count, set) coord = (count + newCount, newSet)
+  where
+    (newCount, newSet) = _basinSize set hmap coord
 
 -- Grid utils
+gridCoords :: [[a]] -> [(Int, Int)]
+gridCoords grid =
+  [(x, y) | y <- [0 .. length (head grid) - 1], x <- [0 .. length grid - 1]]
+
 mapGridWithCoords :: ((Int, Int) -> t -> a) -> [[t]] -> [[a]]
 mapGridWithCoords func grid =
   [[func (x, y) (grid !! x !! y) | y <- [0 .. length (head grid) - 1]] | x <- [0 .. length grid - 1]]
