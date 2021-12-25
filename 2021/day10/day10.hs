a7c366cae8472e7f0d40d9b6b13c4f092c238f31
Retsam <chess19@gmail.com>
2021-12-25 15:06:26 -0500

    Day 10 part 2

--- 2021/day10/day10.hs
+++ 2021/day10/day10.hs
@@ -1,3 +1,5 @@
+import Data.Either
+import Data.List (sort)
 import Debug.Trace
 
 main = do
@@ -5,15 +7,21 @@ main = do
     getContents
   let stuff = lines input
   print $ part1 stuff
+  print $ part2 stuff
 
 part1 :: [String] -> Int
-part1 = sum . map (lineScore [])
+part1 = sum . lefts . map (lineScore [])
+
+part2 :: [String] -> Int
+part2 = middle . sort . map (autocompleteScore . reverse) . rights . map (lineScore [])
+
+middle list = list !! (length list `div` 2)
 
 safeHead :: [a] -> Maybe a
 safeHead list = if null list then Nothing else Just $ head list
 
-lineScore :: String -> String -> Int
-lineScore _ "" = 0
+lineScore :: String -> String -> Either Int String
+lineScore expected "" = Right expected
 lineScore expected (c : cs) = case c of
   '<' -> expect '>'
   '[' -> expect ']'
@@ -30,5 +38,15 @@ lineScore expected (c : cs) = case c of
       Just foundChar ->
         if foundChar == expectedChar
           then lineScore (tail expected) cs
-          else errorScore
-      _ -> errorScore
+          else Left errorScore
+      _ -> Left errorScore
+
+autocompleteScore "" = 0
+autocompleteScore (h : rest) = letterScore + (5 * autocompleteScore rest)
+  where
+    letterScore = case h of
+      ')' -> 1
+      ']' -> 2
+      '}' -> 3
+      '>' -> 4
+      _ -> error "Unexpected"
