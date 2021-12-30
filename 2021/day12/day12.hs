edf71610c79301ce61a3c9b3b666802438fd70f1
Retsam <chess19@gmail.com>
2021-12-29 21:03:58 -0500

    Day 12 part 2

--- 2021/day12/day12.hs
+++ 2021/day12/day12.hs
@@ -1,28 +1,42 @@
+import Control.Monad.Writer
 import Data.Char (toLower)
 import Data.Either (fromRight)
+import Data.List (intercalate, nub)
 import Text.Parsec
 
 main = do
   input <- getContents
   let parsed = parse parseInput "input" input
   let paths = fromRight (error "Parse") parsed >>= pathAndReverse
+
   print $ part1 paths
+  print $ part2 paths
 
 pathAndReverse ("start", b) = [("start", b)]
+pathAndReverse (b, "start") = [("start", b)]
 pathAndReverse (a, "end") = [(a, "end")]
+pathAndReverse ("end", a) = [(a, "end")]
 pathAndReverse (a, b) = [(a, b), (b, a)]
+
+part1 paths = length $ findPath paths part1CanVisit [] "start"
+
+part2 paths = length $ findPath paths part2CanVisit [] "start"
 
 isSmallCave loc = loc == map toLower loc
 
 part1CanVisit prev loc = not (isSmallCave loc && loc `elem` prev)
 
-part1 paths = findPath paths part1CanVisit [] "start"
-
-findPath :: [(String, String)] -> ([String] -> String -> Bool) -> [String] -> String -> Int
-findPath _ _ _ "end" = 1
-findPath paths canVisit prev loc = sum $ map (findPath paths canVisit (loc : prev)) possibleMoves
+hasRepeatSmallCave prev = length smallCaves /= length (nub smallCaves)
   where
-    possibleMoves = filter (canVisit prev) $ map snd (filter ((loc ==) . fst) paths)
+    smallCaves = filter isSmallCave prev
+
+part2CanVisit prev loc = not (hasRepeatSmallCave prev) || part1CanVisit prev loc
+
+findPath :: [(String, String)] -> ([String] -> String -> Bool) -> [String] -> String -> [String]
+findPath _ _ prev "end" = [intercalate "," prev]
+findPath paths canVisit prev loc = mconcat $ map (findPath paths canVisit (loc : prev)) possibleMoves
+  where
+    possibleMoves = filter (canVisit (loc : prev)) $ map snd (filter ((loc ==) . fst) paths)
 
 parseLoc :: Parsec String st String
 parseLoc = many (noneOf "-\n")
