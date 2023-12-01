d524b73d8e4f1c228f1aa63c2c99e8939588a5ac
Retsam <chess19@gmail.com>
2023-12-01 12:29:36 -0500

    [2023, JS] Some cleanup

--- 2023-js/day01/day01.ts
+++ 2023-js/day01/day01.ts
@@ -5,24 +5,25 @@ const input = fs.readFileSync("./input.txt");
 const lines = input.toString().trim().split("\n");
 
 // prettier-ignore
-const digitWords = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+const digitWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
 function solve(isPartTwo: boolean) {
   return lines
     .map((line) => {
-      const chars = line
+      const digits = line
         .split("")
         .map((c, i) => {
           if (parseInt(c)) return parseInt(c);
           if (!isPartTwo) return;
-          const englishDigit = digitWords.findIndex((word) =>
-            line.slice(i).startsWith(word)
+          return (
+            // digitWords is structured so the index of the entry plus one is the value of the word (zero is not a valid digit)
+            // If findIndex is -1, then returns zero, which is removed by filter
+            digitWords.findIndex((word) => line.slice(i).startsWith(word)) + 1
           );
-          return englishDigit > 0 ? englishDigit : undefined;
         })
         .filter((x) => x);
 
-      return parseInt(`${chars[0]}${chars[chars.length - 1]}`);
+      return parseInt(`${digits[0]}${digits.at(-1)}`);
     })
     .reduce((a, b) => a + b);
 }
