5205074d49141b8f2e4f9860f8c3e6a13efd1265
Retsam <chess19@gmail.com>
2023-12-11 11:36:22 -0500

    [2023, Rust] Day 5, part 2

--- 2023/day05/src/main.rs
+++ 2023/day05/src/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::collections::VecDeque;
 use std::io::{self, Read};
 use std::ops::Range;
 
@@ -37,7 +38,15 @@ fn main() -> Result<(), String> {
         .collect::<Vec<_>>();
 
     let part1 = part1(&seeds, &maps);
-    println!("{part1}");
+
+    let pairs = seeds
+        .into_iter()
+        .tuples()
+        .map(|(start, len)| start..(start + len))
+        .collect_vec();
+
+    let part2 = part2(pairs, maps);
+    println!("{part1} {part2}");
 
     Ok(())
 }
@@ -59,6 +68,44 @@ fn part1(seeds: &[u64], mappings: &[Mapping]) -> u64 {
     seeds
         .iter()
         .map(|start| mappings.iter().fold(*start, translate))
+        .min()
+        .unwrap()
+}
+
+fn translate_range(range: Range<u64>, mapping: &Mapping) -> Vec<Range<u64>> {
+    // Find all the start and endpoints of the translations that break up `range`, duplicate them then chunk so we go:
+    // [x1, x2] -> [x1, x1, x2, x2] -> [start, x1, x1, x2, x2, end] -> [start..x1, x1..x2, x2..end]
+    let mut breakpoints = mapping
+        .iter()
+        .flat_map(|(src_range, _)| [src_range.start, src_range.end])
+        .filter(|x| *x > range.start && *x < range.end)
+        .flat_map(|x| [x, x])
+        .collect::<VecDeque<_>>();
+    breakpoints.push_front(range.start);
+    breakpoints.push_back(range.end);
+    breakpoints
+        .into_iter()
+        .sorted()
+        .tuples()
+        .map(|(x, y)| x..y)
+        .collect()
+}
+
+fn part2(ranges: Vec<Range<u64>>, mappings: Vec<Mapping>) -> u64 {
+    mappings
+        .into_iter()
+        .fold(ranges, |ranges, map| {
+            ranges
+                .into_iter()
+                .flat_map(|range| translate_range(range, &map))
+                .map(|range| {
+                    let new_start = translate(range.start, &map);
+                    new_start..(new_start + range.count() as u64)
+                })
+                .collect_vec()
+        })
+        .iter()
+        .map(|range| range.start)
         .min()
         .unwrap()
 }
