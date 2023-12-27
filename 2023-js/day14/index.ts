80a9dc9cc786af0270713121f3cbe5ba11116d85
Retsam <chess19@gmail.com>
2023-12-27 16:33:35 -0500

    [2023, JS] Day 14 solved

---
+++ 2023-js/day14/index.ts
@@ -0,0 +1,83 @@
+import fs from "fs";
+import _ from "lodash";
+
+const input = fs.readFileSync("input.txt").toString().trim();
+
+const lines = input.split("\n");
+
+const data = lines.map((line) => {
+  return line.split("");
+});
+
+const maxX = lines.length;
+const maxY = lines[0].length;
+
+// Moves stones from bottom to top
+const tilt = () => {
+  for (let x = 0; x < maxX; x++) {
+    for (let y = 1; y < maxY; y++) {
+      const tile = data[y][x];
+      const above = data[y - 1][x];
+      if (tile === "O" && above === ".") {
+        data[y][x] = ".";
+        data[y - 1][x] = "O";
+        y -= 1;
+        x -= 1;
+      }
+    }
+  }
+};
+
+const rotate = () => {
+  const originalData = [...data.map((line) => [...line])];
+  for (let x = 0; x < maxX; x++) {
+    for (let y = 0; y < maxY; y++) {
+      data[y][x] = originalData[maxY - x - 1][y];
+    }
+  }
+};
+
+const sum = () => {
+  let sum = 0;
+  for (let x = 0; x < maxX; x++) {
+    for (let y = 0; y < maxY; y++) {
+      if (data[y][x] === "O") sum += maxY - y;
+    }
+  }
+  return sum;
+};
+
+const hash = () => data.map((x) => x.join("")).join("\n");
+
+const cycle = () => {
+  tilt();
+  if (count === 0) console.log(sum());
+  rotate();
+  tilt();
+  rotate();
+  tilt();
+  rotate();
+  tilt();
+  rotate();
+  count++;
+};
+
+let prev = hash();
+
+let count = 0;
+let prevSet = new Map<string, number>([]);
+do {
+  prev = hash();
+  prevSet.set(prev, count);
+  cycle();
+} while (!prevSet.has(hash()));
+const cycleStart = prevSet.get(hash())!;
+const cycleLength = count - cycleStart;
+
+const stepsToTake = (1000000000 - cycleStart) % cycleLength;
+
+for (let i = 0; i < stepsToTake; i++) {
+  cycle();
+}
+
+console.log(sum());
