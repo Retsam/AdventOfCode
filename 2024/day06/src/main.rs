32bb71be921895004a6317a175047a977f4896dc
Retsam <chess19@gmail.com>
2024-12-06 22:50:26 -0500

    [2024, Rust] Day 6, Part 2

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::HashSet,
+    hash::Hash,
     io::{self, Read},
 };
 
@@ -29,12 +30,90 @@ impl Coord {
         }
     }
 }
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
 enum Dir {
     L,
     R,
     U,
     D,
+}
+impl Dir {
+    fn cw(&self) -> Dir {
+        use Dir::*;
+        match self {
+            L => U,
+            U => R,
+            R => D,
+            D => L,
+        }
+    }
+}
+struct Bounds {
+    min_x: i64,
+    min_y: i64,
+    w: i64,
+    h: i64,
+}
+impl Bounds {
+    fn new(w: i64, h: i64) -> Self {
+        Self {
+            min_x: 0,
+            min_y: 0,
+            w,
+            h,
+        }
+    }
+    fn from_vec<T>(stuff: &[Vec<T>]) -> Self {
+        let w = stuff[0].len() as i64;
+        let h = stuff.len() as i64;
+        Bounds::new(w, h)
+    }
+    fn range_x(&self) -> std::ops::Range<i64> {
+        self.min_x..(self.min_x + self.w)
+    }
+    fn range_y(&self) -> std::ops::Range<i64> {
+        self.min_y..(self.min_y + self.h)
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = Coord> {
+        self.range_x()
+            .cartesian_product(self.range_y())
+            .map(|(x, y)| Coord { x, y })
+    }
+    pub fn in_bounds(&self, coord: Coord) -> bool {
+        coord.x >= self.min_x
+            && coord.x < self.min_x + self.w
+            && coord.y >= self.min_y
+            && coord.y < self.min_y + self.h
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum RunResult {
+    Loop,
+    Exit(HashSet<(Coord, Dir)>),
+}
+fn run<F>(start: Coord, bounds: &Bounds, get: F) -> RunResult
+where
+    F: Fn(Coord) -> Option<char>,
+{
+    let mut pos = start;
+    let mut dir = Dir::U;
+    let mut seen = HashSet::<(Coord, Dir)>::new();
+    while bounds.in_bounds(pos) {
+        let already_seen = !seen.insert((pos, dir));
+        if already_seen {
+            return RunResult::Loop;
+        }
+        let next = pos.mv(dir);
+        if let Some('#') = get(next) {
+            dir = dir.cw();
+        } else {
+            pos = next;
+        }
+    }
+
+    RunResult::Exit(seen)
 }
 
 fn main() -> io::Result<()> {
@@ -45,40 +124,36 @@ fn main() -> io::Result<()> {
         .map(|line| line.chars().collect_vec())
         .collect_vec();
 
-    let w = grid[0].len() as i64;
-    let h = grid.len() as i64;
-
-    let grid_iter = (0..grid[0].len() as i64)
-        .cartesian_product(0..grid.len() as i64)
-        .map(|(x, y)| Coord { x, y });
+    let bounds = Bounds::from_vec(&grid);
 
     let get = |c: Coord| {
         grid.get(c.y as usize)
             .and_then(|row| row.get(c.x as usize).cloned())
     };
 
-    let in_bounds = |c: Coord| c.x >= 0 && c.x < w && c.y >= 0 && c.y < h;
+    let start = bounds.iter().find(|c| get(*c).unwrap() == '^').unwrap();
 
-    let mut pos = grid_iter.clone().find(|c| get(*c).unwrap() == '^').unwrap();
-    let mut dir = Dir::U;
-    let mut seen = HashSet::<Coord>::new();
-    while in_bounds(pos) {
-        seen.insert(pos);
-        let next = pos.mv(dir);
-        if let Some('#') = get(next) {
-            dir = match dir {
-                Dir::U => Dir::R,
-                Dir::R => Dir::D,
-                Dir::D => Dir::L,
-                Dir::L => Dir::U,
+    let path = match run(start, &bounds, get) {
+        RunResult::Exit(path) => path.into_iter().map(|(c, _)| c).collect::<HashSet<_>>(),
+        RunResult::Loop => panic!("Loop"),
+    };
+    let part_1 = path.len();
+    let part_2 = path
+        .into_iter()
+        .filter(|&test| {
+            if test == start {
+                return false;
+            }
+            let get_with_block = |c: Coord| {
+                if c == test {
+                    Some('#')
+                } else {
+                    get(c)
+                }
             };
-        } else {
-            pos = next;
-        }
-    }
-
-    let part_1 = seen.len();
-    let part_2 = "";
+            run(start, &bounds, get_with_block) == RunResult::Loop
+        })
+        .count();
 
     println!("Part 1: {part_1}\nPart 2: {part_2}");
 
