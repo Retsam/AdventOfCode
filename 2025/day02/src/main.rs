151f7c69e28b613cdd9d800b925196cbfa2fc97e
Retsam <chess19@gmail.com>
2025-12-02 13:54:46 -0500

    [2025, Rust] Day 2, part 2

--- 2025/day02/src/main.rs
+++ 2025/day02/src/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::collections::HashSet;
 use std::error;
 use std::io::{self, Read};
 
@@ -21,51 +22,63 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         })
         .collect::<Vec<_>>();
 
-    let (mut p1, p2) = (0, 0);
-
-    let double = |n: u64| {
-        let s = n.to_string();
-        format!("{s}{s}").parse::<u64>().unwrap()
-    };
-    let half = |n: u64| {
-        let s = n.to_string();
-        let len = s.len();
-        n.to_string()
-            .chars()
-            .take((len / 2) as usize)
-            .collect::<String>()
-            .parse::<u64>()
-            .unwrap()
-    };
+    let (mut p1, mut p2) = (0, 0);
 
     for (lower, upper) in ranges {
-        let in_range = |n: u64| (lower..=upper).contains(&n);
-        let is_even = |n| n % 2 == 0;
+        // Collect candidates in a set - we can't double count something like 222222 as both a double and a triple (and a 6-tuple)
+        let mut candidates = solve(lower, upper, 2);
+        p1 += candidates.iter().sum::<u64>();
 
-        let lower_len = lower.ilog10() + 1;
-        let upper_len = upper.ilog10() + 1;
-
-        let val = if is_even(lower_len) {
-            let first_half = half(lower);
-            if in_range(double(first_half)) {
-                Some(first_half)
-            } else {
-                Some(first_half + 1)
-            }
-        } else if is_even(upper_len) {
-            Some(10u64.pow(upper_len / 2 - 1))
-        } else {
-            None
-        };
-        if let Some(mut val) = val {
-            while in_range(double(val)) {
-                p1 += double(val);
-                val += 1;
-            }
+        for size in 3..=upper.to_string().len() {
+            candidates.extend(solve(lower, upper, size));
         }
+        p2 += candidates.iter().sum::<u64>();
     }
 
     println!("Part 1: {p1}\nPart 2: {p2}");
 
     Ok(())
+}
+
+fn repeat(s: u64, times: usize) -> u64 {
+    std::iter::repeat(s.to_string())
+        .take(times)
+        .collect::<Vec<_>>()
+        .join("")
+        .parse()
+        .unwrap()
+}
+
+fn solve(lower: u64, upper: u64, size: usize) -> HashSet<u64> {
+    let lower_str = lower.to_string();
+    let upper_str = upper.to_string();
+    let lower_len = lower_str.len();
+    let upper_len = upper_str.len();
+
+    // Early return if neither bound is a multiple of size
+    // (The input does not include ranges across more than one order of magnitude, e.g. 99-101 is possible, but 99-1010 is not)
+    if lower_len % size != 0 && upper_len % size != 0 {
+        return HashSet::new();
+    }
+
+    let lower_bound = if lower_len % size == 0 {
+        let start = &lower_str[0..(lower_len / size)];
+        start.parse::<u64>().unwrap()
+    } else {
+        // e.g. 888 - 2000, with size 2, the lower-limit is 10 (i.e. 1010)
+        10u64.pow(upper_len as u32 / 2 - 1)
+    };
+    let upper_bound = if upper_len % size == 0 {
+        let start = &upper_str[0..(upper_len / size)];
+        start.parse::<u64>().unwrap()
+    } else {
+        // e.g. 8888-10100, with size 2, the upper-limit is 99 (i.e. 9999)
+        10u64.pow(lower_len as u32 / 2) - 1
+    };
+
+    (lower_bound..=upper_bound)
+        .into_iter()
+        .map(|n| repeat(n, size))
+        .filter(|candidate| (lower..=upper).contains(candidate))
+        .collect()
 }
